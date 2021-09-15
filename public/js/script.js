53a19b77b738f3f77c6f3333b151a463f1663f5f
Mareedu Saibabu <marsaiba@publicisgroupe.net>
2021-09-15 13:00:50 +0530

    .Palindrome checker

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,28 @@
 const inputValue = document.querySelector('input');
 const btnCheck = document.querySelector('button');
 
+btnCheck.addEventListener('click', (event) => {
 
+    const inputWord = inputValue.value.toLocaleLowerCase();
+    console.log(inputWord);
+
+    const string = inputWord.split("");
+
+
+    const revArray = string.reverse();
+    console.log(revArray);
+
+    const joinArray = revArray.join("");
+    console.log(joinArray);
+
+    if(inputWord == joinArray){
+    document.querySelector('.output-text').innerHTML = `
+    "${inputWord} = ${joinArray}" - is a Palindrome `;
+
+    }
+    else {
+        document.querySelector('.output-text').innerHTML = `
+        "${inputWord} = ${joinArray}" - is not Palindrome `;
+    }
+})
 
